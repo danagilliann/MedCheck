47660286356aedf1b8dbafd0d3c48154f55d6613
Viktor <mail@viktor-schlueter.de>
2015-09-27 05:36:22 -0400

    new front dev

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/weekview', function (req, res) {
+	res.sendFile(path.join(__dirname + '/views/weekview.html'));
+});
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
